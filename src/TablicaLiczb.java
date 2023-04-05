c46c327c52b9b27ff788fb24e03b81d202ea8384
Camprite <u19_dawidborkowski1@zsp1.siedlce.pl>
2023-04-05 21:15:49 +0200

    Zadanie7 nie optymalnie ale działa

--- src/TablicaLiczb.java
+++ src/TablicaLiczb.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TablicaLiczb {
     int[] tablica = null;
     private int length;
@@ -7,22 +10,73 @@ public class TablicaLiczb {
         this.tablica = new int[length];
     }
     public void wpiszDoTablicyZZakresu(int odLiczby, int doLiczby){
-        for (int i:tablica
-             ) {
-            tablica[i]= (int)(Math.random() * doLiczby) + odLiczby;
+        for (int i = 0; i< length; i++)  {
+            tablica[i]= (int)Math.floor((Math.random() * doLiczby) + odLiczby);
         }
     }
     public void wpiszDoTablicyZZakresu(){
-        for (int i:tablica
-             ) {
-            tablica[i]= (int)(Math.random() * 1000000000) + 1;
+        for (int i = 0; i< length; i++) {
+            tablica[i] = (int)Math.floor((Math.random() * 10) + 1);
         }
     }
     public void wypiszTablice(){
-        for (int i:tablica
-        ) {
-            System.out.println(tablica[i]);
+        for (int i:this.tablica) {
+            System.out.println(i);
         }
+    }
+    public void zapiszLiczebnoscElementów(TablicaLiczb tab2) throws IOException {
+        FileWriter file = new FileWriter("src/liczebnosc.txt");
+        for(int i = 0;i<this.length; i++){
+            int value = this.tablica[i];
+            boolean isBefore = false;
+            for (int j = 0; j<i; j++){
+                if(tablica[j]==value){
+                    isBefore = true;
+                }
+            }
+            int score = 1;
+            for (int j = i+1; j<this.length; j++){
+                if(this.tablica[j] == value){
+                    score++;
+                }
+            }
+            for (int j = 0; j<tab2.length; j++){
+                if(tab2.tablica[j] == value){
+                    score++;
+                }
+            }
+            if(!isBefore){
+            file.write(value + " " + score + "\n");
+        }}
+        for(int i = 0;i<tab2.tablica.length; i++){
+            int score = 0;
+            for (int j = 0; j<this.length; j++){
+                if(tab2.tablica[i] == tablica[j]){
+                    score++;
+                }
+            }
+            if(score==0){
+                int value = tab2.tablica[i];
+                int liczebnosc = 1;
+                boolean isBefore = false;
+                for (int j = 0; j<i; j++){
+                    if(tab2.tablica[j]==value){
+                        isBefore = true;
+                    }
+                }
+                for(int j = i+1 ; j< tab2.tablica.length;j++){
+                    if(value == tab2.tablica[j]){
+                        liczebnosc++;
+                    }
+                }
+                if(!isBefore){
+                    file.write(value + " " + liczebnosc + "\n");
+                }
+            }
+
+        }
+        file.close();
+
     }
 
 
--- src/zadanie7.java
+++ src/zadanie7.java
@@ -1,12 +1,20 @@
+import java.io.IOException;
+
 public class zadanie7 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TablicaLiczb  tablica1 = null;
         TablicaLiczb  tablica2 = null;
         try{
             tablica1 = new TablicaLiczb(10);
-            tablica2 = new TablicaLiczb(100);
+            tablica2 = new TablicaLiczb(20);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        tablica1.wpiszDoTablicyZZakresu();
+        tablica2.wpiszDoTablicyZZakresu();
+        tablica1.wypiszTablice();
+        System.out.println("Tab2");
+        tablica2.wypiszTablice();
+        tablica1.zapiszLiczebnoscElementów(tablica2);
     }
 }
