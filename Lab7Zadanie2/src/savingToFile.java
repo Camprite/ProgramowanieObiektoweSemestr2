47b75f7ed0a9cdb8c7b1f55f9791ae4823e6f8ab
Camprite <u19_dawidborkowski1@zsp1.siedlce.pl>
2023-04-07 21:43:45 +0200

    Oczyt/zapis do napisania na nowo :c

--- Lab7Zadanie2/src/savingToFile.java
+++ Lab7Zadanie2/src/savingToFile.java
@@ -7,49 +7,70 @@ import java.io.FileInputStream;
 
 public class savingToFile {
     private savingToFile(){
-
     }
-
-
-    public static void zapisz(String fileName, ArrayList list) throws IOException {
-        ObjectOutputStream saving = new ObjectOutputStream(new FileOutputStream(fileName));
+    public static void zapisz(String fileName, ArrayList list,boolean append) throws IOException {
+        ObjectOutputStream saving = new ObjectOutputStream(new FileOutputStream(fileName,append));
         list.forEach((o) -> {
             try {
                 saving.writeObject(o);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        });
+        }
+        );
         saving.close();
-
     }
-    public static void odczyt(String filename) throws IOException, ClassNotFoundException {
-        System.out.println("Odczytstart");
-//        ObjectInputStream o = null;
-//        Kierownik obj = null;
-//        do {
-//            o = new ObjectInputStream
-//                    ( new FileInputStream(filename));
-//            obj = (Kierownik) o.readObject();
-//            obj.wyswietlDane();
-//
-//        }while (obj!=null);
 
-        ArrayList<Object> objectsList = new ArrayList<>();
+    public static void odczyt(String filename) throws IOException, ClassNotFoundException {
+
+        System.out.println("Odczytstart");
+        ArrayList<Kierownik> KierownikList = new ArrayList<>();
+        ArrayList<Pracownik> PracownikList = new ArrayList<>();
         boolean cont = true;
+        Object obj = null;
+        ObjectInputStream input = new ObjectInputStream(new FileInputStream(filename));
         while (cont) {
-            try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(filename))) {
-                Object obj = input.readObject();
+            System.out.println("ITERACJA");
+            try{
+
+                obj = input.readObject();
                 if (obj != null) {
-                    objectsList.add(obj);
-                } else {
+//                    if(1==1){
+//                        System.out.println("TAK");
+////                        KierownikList.add((Kierownik) obj);
+////                        ((Kierownik) obj).wyswietlDane();
+//                    }
+//                    if(2==2){
+                        PracownikList.add((Pracownik) obj);
+//                        ((Pracownik) obj).wyswietlDane();
+//                    }
+                }
+                if(obj == null){
                     cont = false;
                 }
+
             } catch (Exception e) {
-                // System.out.println(e.printStackTrace());
+                System.out.println(e);
+                e.printStackTrace();
+                System.out.println("Koniec pliku");
+                cont = false;
             }
         }
         System.out.println("Odczytend");
+        System.out.println("");
+        System.out.println("KIEROWNICY Z PLIKU");
+        System.out.println("");
+        KierownikList.forEach((e)->{
+            e.wyswietlDane();
+        });
+
+        System.out.println("");
+        System.out.println("PRACOWNICY Z PLIKU");
+        System.out.println("");
+        PracownikList.forEach((f)->{
+            f.wyswietlDane();
+        });
     }
 
 }
--- Lab7Zadanie2/src/zadanie2.java
+++ Lab7Zadanie2/src/zadanie2.java
@@ -5,9 +5,9 @@ public class zadanie2 {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Pracownik prac1 = new Pracownik("Andrzej","Duda","01.01.1980","04.10.2012",3300,"Marketing");
         Pracownik prac2 = new Pracownik("Jerzy","Szybki","02.03.2001","10.22.2022",1300,"PR");
-        Pracownik prac3 = new Pracownik("Janusz","Nosacz","17.05.1970","01.06.2010",2500,"Oszczędności");
-        Kierownik kier1 = new Kierownik("Chad","Giga","28.07.1990","25.04.2018",5000,"Rozwój",10,"Manager",50);
-        Kierownik kier2 = new Kierownik("Mieczysław","Zamożny","24.06.1996","20.05.1999",7000,"Zaopatrzenie",4,"Syn właściciela",70);
+        Pracownik prac3 = new Pracownik("Janusz","Nosacz","17.05.1970","01.06.2010",2500,"Oszczednosci");
+        Kierownik kier1 = new Kierownik("Chad","Giga","28.07.1990","25.04.2018",5000,"Rozwoj",10,"Manager",50);
+        Kierownik kier2 = new Kierownik("Mieczyslaw","Zamozny","24.06.1996","20.05.1999",7000,"Zaopatrzenie",4,"Syn wlasciciela",70);
 //        Pracownik prac4 =  kier1.nowyPracownik();
 //        Pracownik prac5 = kier2.nowyPracownik();
 //        kier1.wyswietlDane();
@@ -22,9 +22,11 @@ public class zadanie2 {
         kierownicy.add(kier1);
         kierownicy.add(kier2);
 
-//        savingToFile.zapisz("Lab7Zadanie2/src/pracownicy.ser",pracownicy);
-        savingToFile.zapisz("Lab7Zadanie2/src/pracownicy.ser",kierownicy);
-        savingToFile.odczyt("Lab7Zadanie2/src/pracownicy.ser");
+
+        String filename = "Lab7Zadanie2/src/pracownicy.ser";
+        savingToFile.zapisz(filename,pracownicy,false);
+//        savingToFile.zapisz(filename,kierownicy,true);
+        savingToFile.odczyt(filename);
 //        prac1.wyswietlDane();
     }
 }
